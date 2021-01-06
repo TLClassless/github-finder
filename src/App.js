04ee3836c8425289ec63afaf112b6ff69f27357b
Ben <classle55sc2@gmail.com>
2021-01-06 13:50:34 +0000

    end of lecture 18

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
 
   // Search GH Users
   searchUsers = async (text) => {
+    this.setState({ loading: true });
     const res = await Axios.get(
       `https://api.github.com/search/users?q=${text}&?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
